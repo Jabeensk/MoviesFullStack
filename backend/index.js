2361e119a09efa0ca7ea037fd027b1f433eb68db
Jabeen <jabeensyed@gmail.com>
2024-02-02 11:02:01 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import './loadEnv.js';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import moviesRouter from './routes/movies.js'
+import usersRouter from './routes/users.js'
+
+console.log(process.env.ATLAS_URI);
+
+const app = express ()
+const PORT = process.env.PORT || 4000;
+
+// middlewares
+app.use(cors()); // allows frontend to connect to backend
+app.use(morgan('dev')); // logger
+app.use(express.json()); // for data in req.body
+app.use(express.urlencoded({extended: true})); // allow data in url string
+
+//Routes
+app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
+
+app.get('/', (req, res) => {
+    res.send('backend...')
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
